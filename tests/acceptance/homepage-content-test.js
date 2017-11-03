e512bae1023e183b11ad902d9adffbf591ee42d0
Kevin Solorio <soloriok@gmail.com>
2017-11-02 21:18:45 -0700

    begin writing tests for homepage content;

---
+++ tests/acceptance/homepage-content-test.js
@@ -0,0 +1,11 @@
+import { test } from 'qunit';
+import moduleForAcceptance from 'moon/tests/helpers/module-for-acceptance';
+
+moduleForAcceptance('Acceptance | homepage content');
+
+test('homepage should show all lunar projects', function(assert) {
+  visit('/')
+  andThen(() => {
+    assert.equal(find('.missions').length, 3, 'display 3 lunar projects')
+  })
+})
