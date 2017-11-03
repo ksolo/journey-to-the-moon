1d51389cda3f611937fb79a62e73dddb6895d4e5
Kevin Solorio <soloriok@gmail.com>
2017-11-02 20:29:04 -0700

    update cli build (typos)

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -8,9 +8,6 @@ module.exports = function(defaults) {
     // Add options here
   });
 
-  app.import('node_modules/materialize-css/dist/css/materialize.min.css')
-  app.import('node_modules/materialize-css/dist/js/materrialize.min.js')
-
   // Use `app.import` to add additional libraries to the generated
   // output files.
   //
@@ -23,6 +20,10 @@ module.exports = function(defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
+
+  app.import('node_modules/materialize-css/dist/css/materialize.min.css')
+  app.import('node_modules/materialize-css/dist/js/materialize.min.js')
+
 
   return app.toTree();
 };
