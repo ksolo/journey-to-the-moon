665853c66ac3cd07b140bac6cf3b62286cc11b5a
Kevin Solorio <soloriok@gmail.com>
2017-11-25 22:25:49 -0500

    add factories

---
+++ mirage/factories/project.js
@@ -0,0 +1,8 @@
+import { Factory } from 'ember-cli-mirage';
+
+export default Factory.extend({
+  name: 'Apollo',
+  startingYear: '1961',
+  endingYear: '1972',
+  logoURL: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/00/Apollo_program.svg/619px-Apollo_program.svg.png'
+});
